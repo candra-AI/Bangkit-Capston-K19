5f09727aa1028d27148acb167ce270a3d47fd44e
Fadillah Mozhaiq <mozhaiqramadhani05@gmail.com>
2021-06-02 15:37:44 +0800

    Membuat View Binding

--- app/src/main/java/id/capstone/project/skindetector/ui/fragment/signup/SignUpFragment.kt
+++ app/src/main/java/id/capstone/project/skindetector/ui/fragment/signup/SignUpFragment.kt
@@ -6,6 +6,8 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import id.capstone.project.skindetector.R
+import id.capstone.project.skindetector.databinding.FragmentSignUpBinding
+import id.capstone.project.skindetector.databinding.FragmentWelcomeBinding
 
 /**
  * A simple [Fragment] subclass.
@@ -13,11 +15,19 @@ import id.capstone.project.skindetector.R
  * create an instance of this fragment.
  */
 class SignUpFragment : Fragment() {
+    private var _binding: FragmentSignUpBinding? = null
+    private val binding get() = _binding!!
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_sign_up, container, false)
+        _binding = FragmentSignUpBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 }
